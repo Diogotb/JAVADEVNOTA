ec119a7ecb3f723dc67fc7f6d145106893741d23
Diogotb <digogo@gmail.com>
2023-04-10 20:11:46 -0300

    10/04/23

---
+++ ExerciciosVetores/ExercicioVetores.java
@@ -0,0 +1,20 @@
+package ExerciciosVetores;
+
+public class ExercicioVetores {
+    public void exercicio1() {
+        int vetorA[]= new int[]{5,10,15,20,25};
+        int vetorB[]= new int[5];
+        vetorB[0]=vetorA[0];
+        vetorB[1]=vetorA[1];
+        vetorB[2]=vetorA[2];
+        vetorB[3]=vetorA[3];
+        vetorB[4]=vetorA[4];
+        System.out.println("o vetorB é");
+        System.out.println(vetorB[0]);
+        System.out.println(vetorB[1]);
+        System.out.println(vetorB[2]);
+        System.out.println(vetorB[3]);
+        System.out.println(vetorB[4]);
+
+        
+}
