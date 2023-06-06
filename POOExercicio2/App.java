455346cc2e68a94b96063d42018023fdd0a421e1
Diogotb <digogo@gmail.com>
2023-06-06 13:15:28 -0300

    06/06

---
+++ POOExercicio2/App.java
@@ -0,0 +1,36 @@
+package POOExercicio2;
+
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class App {
+    public static void main(String[] args) {
+        Random rd = new Random();
+        //criar um array de objetos
+        Agenda contatos[] = new Agenda[10];
+        //criar e preencher os objetos
+        for (int i = 0; i < contatos.length; i++) {
+            contatos[i] = new Agenda();
+            //preencher os atributos do objjeto
+            contatos[i].setNome(JOptionPane.showInputDialog("Nome:"));
+            contatos[i].setIdade(i+18);
+            contatos[i].setAltura(rd.nextDouble(1.5,2));
+        }
+        //busca de um objetos da Agenda(nome)
+        String nomeBuscado = JOptionPane.showInputDialog(
+                            "Informe o Nome Buscado");
+        int cont=0;// contador
+        boolean procure = true;
+        while(procure){//enquanto procure for verdadeiro loop
+            if(nomeBuscado.equals(contatos[cont].getNome())){
+                procure = false;//parar o laço
+                JOptionPane.showMessageDialog(null,
+                    "Idade: "+contatos[cont].getIdade()
+                    +"\nAltura: "+contatos[cont].getAltura());
+            }
+            cont++;
+        }
+
+    }
+}
--- POOExrcicio2/Agenda.java
+++
@@ -1,8 +0,0 @@
-package POOExrcicio2;
-
-public class Agenda {
-    String nome;
-    int idade;
-    double altura;
-
-}
